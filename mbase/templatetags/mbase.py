da25765df21a33f0e80aeccc53db6175a1340ca1
Win <synwe@yahoo.com>
2017-06-14 13:25:18 +0000

    Update templatetags and cleanup

--- mbase/templatetags/mbase.py
+++ mbase/templatetags/mbase.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django import template
+from django.conf import settings
 from django.template.defaultfilters import stringfilter
 
 register = template.Library()
@@ -9,5 +10,11 @@ register = template.Library()
 @stringfilter
 def in_admin(path, autoescape=True):
     if path.startswith("/admin"):
+        return True
+    return False
+
+@register.filter
+def is_installed(vvapp):
+    if vvapp in settings.INSTALLED_APPS:
         return True
     return False
